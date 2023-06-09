3ef28f3e98a30856c8504d67b818aa8683761d35
Wouter Vernaillen <wouter@vernaillen.com>
2023-06-09 13:09:44 +0200

    fix i18n setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,5 +23,20 @@ export default defineNuxtConfig({
         }
     },
 
-    i18n,
+    i18n: {
+        detectBrowserLanguage: {
+            useCookie: true,
+            redirectOn: 'root'
+        },
+        locales: [
+            { code: 'nl', iso: 'en-BE', file: 'nl-BE.json' },
+            { code: 'en', iso: 'en-US', file: 'en-US.json' }
+        ],
+        langDir: 'locales/',
+        defaultLocale: 'nl',
+        lazy: true,
+        strategy: 'prefix_and_default',
+
+        vueI18n: './config/i18n.config.ts'
+    }
 })
